f77facf5ae0da7681bb4e282dc101dc40e85ee53
Anouxay Sichanh <ainthaly@gmail.com>
2025-03-07 09:22:34 -0500

    css mobile share icon pos. relative fix

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ button.addEventListener('click', () => {
     socialLinks.classList.toggle('active');
 
     if (button.classList.contains('share-active')) {
-        button.classList.toggle.remove('share-active', 'share-active-img');
+        button.classList.remove('share-active', 'share-active-img');
     } else {
         button.classList.add('share-active', 'share-active-img');
     }
